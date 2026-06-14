0aad2d32108b5220927399262a86356973f2a5b2
aXun-2 <3500567695@qq.com>
2026-06-14 17:43:22 -0400

    login/signup feature + user profile

--- app/app/src/main/java/com/example/flashappcs4520/MainActivity.kt
+++ app/app/src/main/java/com/example/flashappcs4520/MainActivity.kt
@@ -4,15 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.flashappcs4520.data.SupabaseProvider
 import com.example.flashappcs4520.ui.ArticleViewModel
 import com.example.flashappcs4520.ui.FeedScreen
 import com.example.flashappcs4520.ui.LoginScreen
+import com.example.flashappcs4520.ui.ProfileScreen
 import com.example.flashappcs4520.ui.SignUpScreen
 import com.example.flashappcs4520.ui.theme.FlashAppCS4520Theme
-import io.github.jan.supabase.auth.auth
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +22,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FlashAppCS4520Theme {
-                var screen by remember { mutableStateOf(
-                    if (SupabaseProvider.client.auth.currentUserOrNull() != null) "feed"
-                    else "login"
-                )}
+                var screen by remember { mutableStateOf("login") }
 
                 when (screen) {
                     "login" -> LoginScreen(
@@ -36,8 +35,16 @@ class MainActivity : ComponentActivity() {
                     )
                     "feed" -> {
                         val articleViewModel: ArticleViewModel = viewModel()
-                        FeedScreen(articleViewModel = articleViewModel)
+                        FeedScreen(
+                            articleViewModel = articleViewModel,
+                            onProfileClick = { screen = "profile" },  // add this
+                        )
                     }
+                    "profile" -> ProfileScreen(
+                        userName = "username",
+                        interests = listOf("Technology", "Finance", "World News"),
+                        onBackClick = { screen = "feed" },
+                    )
                 }
             }
         }
